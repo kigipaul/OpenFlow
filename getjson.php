a132cd23633c12c4cb618472f9edc98db4b9d5c0
kigipaul <kigipaul@gmail.com>
2013-01-25 11:21:51 +0800

    Modify two things / Modify json_array() Schema: add 10 min Counter Gap / Add get Counter_Base and modify compare to this Base Value

--- getjson.php
+++ getjson.php
@@ -7,13 +7,16 @@ $JSON_HOST = "127.0.0.1";
 $JSON_PORT = "8080";
 $JSON_ROOT = "wm";
 $JSON_PATH = "$JSON_HOST:$JSON_PORT/$JSON_ROOT";
-
+$BASE_PATH = "./";
 
 $JSON_DATA="";
 ##
 ## Get Param
 ##
 
+if(!isset($_GET['t'])){
+	exit(0);
+}
 $TYPE=$_GET['t'];
 
 if($TYPE=='device'){
@@ -29,8 +32,9 @@ if($TYPE=='device'){
 	
 	$counter_array = Counter_jsonToArray($src_json_data);
 	
-	
 	$data_layout = Counter_layout($counter_array);
+
+	$getBase = Counter_getBase($BASE_PATH);
 	##
 	##	Compare data and make json
 	##	$json_array Schema:
@@ -47,7 +51,7 @@ if($TYPE=='device'){
 	##		like $json_array
 	##		[
 	##			{"switchID":switchDPID,
-	##			 "interface":[{"port":port#,"level":[0-10]}]
+	##			 "interface":[{"port":port#,"level":[0-10],"counter":10min Counter Gap}]
 	##			}
 	##		]
 	##
@@ -61,8 +65,9 @@ if($TYPE=='device'){
 		$tmp_data_arr['switchID'] = $switchID;
 		$tmp_data_arr['interface'] = array();
 		foreach($port as $port_num => $val){
-			$level = ($val>=$upper_bound?8:0);
-			$tmp_arr = array("port"=>$port_num,"level"=>$level);
+			$counter_gap = $val-$getBase[$switchID][$port_num];	
+			$level = ($counter_gap>=$upper_bound?8:0);
+			$tmp_arr = array("port"=>$port_num,"level"=>$level,"counter"=>$counter_gap);
 			array_push($tmp_data_arr['interface'],$tmp_arr);
 		}
 		array_push($json_array,$tmp_data_arr);
